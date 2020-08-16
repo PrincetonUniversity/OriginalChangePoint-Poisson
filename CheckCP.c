f8a24d2d621b8e9d22c1a5954fb6a3d914b26770
Haw Yang <hawyang@princeton.edu>
2020-08-16 19:50:17 -0400

    CheckCP.c cleaned up

--- CheckCP.c
+++ CheckCP.c
@@ -1,27 +1,25 @@
 /***************************************************************************
                           CheckCP.c  -  description
                              -------------------
-    begin                : Wed Jan 28 2004
-    last modified        : Wed Jan 28 2004
-    copyright            : (C) 2004-2012 by Haw Yang
-    email                : hawyang@princeton.edu
- ***************************************************************************/
+    begin coding                : Tue Jan 27 14:25:30 PST 2004
+    peer-reviewed publication   : J. Phys. Chem. B, 109, 617-628 (2005)
+    code initial public release : 2020
+    copyright                   : © Haw Yang 2020
+    email                       : hawyang@princeton.edu
+***************************************************************************/
+
+// Change log (for public release):
+// 20200811: (HY) Start code clean up for v2.00 initial public release.
 
 // References:
-// 1.
-// 2. "A problem with the likelihood ratio test for a change-point hazard rate model,"
+// 1. "A problem with the likelihood ratio test for a change-point hazard rate model,"
 //    Robin Henderson, Biometrika, 77, 835-843 (1990).
 //
 // Change Log:
-// 20120512: (HY) Implement a bug fix at line ca. 83, credited to Arno van Amersfoort
-//                fixed a printf specifier bug for size_t variables
-//                replacing %i with %lu
-// 20040326: (HY) Implement Henderson's static using exact distribution.
-//                As of now, this subroutine still cannot cope with data length longer
-//                than critical value length
-// 20040206: (HY) implement modifications by Robin Henderson Ref. [2]
-// 20040203: (HY) need to add a clause to remove points whose neighboring
-//           change points are within its uncertainty.
+// 20130919: (DPR) Bug fix at line ca. 78 by Ducan P. Ryan for potential inifinite loop  
+//                 caused by change-point intervals greater than Na window size.
+// 20120512: (AvA) Implement a bug fix at line ca. 115, credited to Arno van Amersfoort
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -110,24 +108,22 @@ int                 trace;
 	  */
 	  cp1=FindCP(cp_root,traj,winl,winr,alpha,beta,&Ncp,ta,ca,Na,3); 
       if (cp1==0){
- 	    printf("    %lu/%i Checking node %lu %lu %lu...",(long unsigned int) i,Ncp,(long unsigned int) winl, (long unsigned int) cps[i], (long unsigned int) winr);
-        printf("not found! RESTARTING\n",cp1);
+        printf("    %lu/%i Checking node %lu %lu %lu...",(long unsigned int) i,Ncp,(long unsigned int) winl, (long unsigned int) cps[i], (long unsigned int) winr);
+        printf("not found! RESTARTING\n");
         deletion_adjustment=true;
         (*Ncpdlt)++;
-        //iend=((i-10)>1 ? i : 10); // This is a buggy statement
         iend = (i > 10) ? i : 10; // bug fix credited to Arno van Amersfoort
-		//printf("%i ",iend);
-		last_winr=0; // clear window
+        last_winr=0; // clear window
         break;
         }
       else if(cp1!=cps[i]){
-	    printf("    %lu/%i Checking node %lu %lu %lu...",(long unsigned int) i,Ncp,(long unsigned int) winl, (long unsigned int) cps[i], (long unsigned int) winr);
+        printf("    %lu/%i Checking node %lu %lu %lu...",(long unsigned int) i,Ncp,(long unsigned int) winl, (long unsigned int) cps[i], (long unsigned int) winr);
         printf("found at %lu.\n",(long unsigned int) cp1);
         deletion_adjustment=true;
         iend=i;
         break;
         }
-	  last_winr=0; // clear window
+        last_winr=0; // clear window
       }
     free(cps);
     free(cpsl);
@@ -139,166 +135,3 @@ int                 trace;
     }
 }
 
-/*
-
-  while ( Ncp > 0 && deletion_adjustment ) {
-    for (j=1;j<=Ncp;j++) {
-      n = cps[j+1] - cps[j-1];
-      if ( n < 1 ) {
-        // remove this spurious point at index j
-        // increase the number of deleted point by 1
-        (*Ncpdlt)++;
-        // now, delete the cp node
-        if (trace==1) printf( "  -=> deleting adjacent transition at index %i\n", cps[j] );
-        DeleteCPNode(cps[j],cp_root,&h);
-        // The next step may take a lot of time, using a bi-directional chain
-        // data structure may help to speed up the process.
-        // move up the working array
-        for (i=j;i<=Ncp;i++) {
-          cps[i] = cps[i+1];
-          cpsl[i] = cpsl[i+1];
-          cpsr[i] = cpsr[i+1];
-          }
-        Ncp--;
-        deletion_adjustment=true;
-        break;
-        }
-      else{ // check if this point is good
-        // create work space
-        Tk = (double *) malloc( (n+1) * sizeof(double) );
-        lambda1 = (double *) malloc( (n+1) * sizeof(double) );
-        lambda2 = (double *) malloc( (n+1) * sizeof(double) );
-        llrt = (double *) malloc( (n+1) * sizeof(double) );
-        u = (double *) malloc( n * sizeof(double) );
-        u1 = (double *) malloc( n * sizeof(double) );
-        v2 = (double *) malloc( n * sizeof(double) );
-        v21 = (double *) malloc( n * sizeof(double) );
-        sk = (double *) malloc( n * sizeof(double) );
-        // total time in this trajectory segment
-        T = traj[cps[j+1]].time - traj[cps[j-1]].time;
-        lambda0 = (double) n / T + DBL_EPSILON;
-        // calculate the critical region
-        //critical_region = C_ac(alpha,n);
-        critical_region = ta[n]; // use tabulated *exact* values
-        // compute expection values and standard deviations
-        for (i=1; i<n; i++)
-          u[i] = u1[i] = v2[i] = v21[i] = 0.0;
-        for (i=1; i<n; i++) {
-          iL = (double) i;
-          for (k=i; k<n; k++) {
-            kL = (double) k;
-            u[i] -= 1.0/kL;                         // expection value for log(Vk)
-            v2[i] += 1.0/kL/kL;                     // variance for [log(Vk)]^2
-            }
-          }
-        for (i=1; i<n; i++) {
-          u1[n-i] = u[i];                           // expection value for log(1-Vk)
-          v21[n-i] = v2[i];                         // variance for [log(1-Vk)]^2
-          }
-        nL = (double) n;
-        term = M_PI*M_PI/6.0-v2[1];
-        for (i=1; i<n; i++) {
-          iL = (double) i;
-          sk[i] = 4.0*iL*iL*v2[i]+4.0*(nL-iL)*(nL-iL)*v21[i]-8.0*iL*(nL-iL)*term;
-          sk[i] = sqrt(sk[i]);
-          }
-        for ( k=1,k_max=0,llrt_max=0.0; k<n; k++ ) {
-          kL = (double) k;
-          Tk[k] = traj[cps[j-1]+k].time - traj[cps[j-1]].time;
-          // Henderson's standardization
-          Vk = Tk[k] / T;
-          llrt[k] = (-2.0*kL*log(Vk)+2.0*kL*u[k]-2.0*(nL-kL)*log(1.0-Vk)
-                    +2.0*(nL-kL)*u1[k])/sk[k]+0.5*log(4.0*kL*(nL-kL)/nL/nL);
-          // Generalized Likelihood Ratio Test
-
-          lambda1[k] = k / Tk[k] + DBL_EPSILON;
-          lambda2[k] = (double) (n-k) / (T-Tk[k]) + DBL_EPSILON;
-          llrt[k] = (double) k*log(lambda1[k]) + (double) (n-k)*log(lambda2[k])
-                  - (double) n*log(lambda0);
-          llrt[k] = 2.0 * llrt[k];
-                  //+ log((double)4*k*(n-k)/n/n); // this weighting term follows Ref[2]
-
-         if ( llrt[k] > llrt_max ) {
-            // find the maximum of the likelihood functions
-            llrt_max = llrt[k];
-            k_max = k;
-            }
-          }
-
-        if ( llrt_max > critical_region ) {
-          // Is it the same as cps[j] ?
-          //if ( (k_max+cps[j-1] <= cpsr[j]) && (k_max+cps[j-1] >= cpsl[j]) )
-          if ( k_max+cps[j-1] == cps[j] )
-            // within the confidence interval
-            deletion_adjustment = false;
-          else { // Not the same as cps[j], adjust it
-            if (trace==1) printf( "] %i ==> [%u] (%u) [%u] in [%u .. %u]\n",
-                                   j, cpsl[j], k_max+cps[j-1], cpsr[j], cps[j-1], cps[j+1] );
-            // max{llrt} is the refined change point
-            // find the confidence interval, threshold given by ca
-            // find the left-hand bound
-            k = k_max;
-            printf( "k_max = %u, llrt[k]+ca[n]-llrt_max = %le\n", k_max, llrt[k]+ca[n]-llrt_max );
-            printf( "ca(n) = %le\n", ca[n] );
-            while ( (k>0) && (llrt[k]+ca[n]-llrt_max > 0) ) k--;
-            LB = k;
-            printf( "LB = %u, n=%u\n", LB, n );
-            // find the right-hand bound
-            k = k_max;
-            while ( (k<n) && (llrt[k]+ca[n]-llrt_max > 0) ) k++;
-            RB = k;
-
-            if (trace==1) printf( "  ~=> adjusting cps[%i] = %i to %i,\n", j, cps[j], k_max+cps[j-1] );
-            h = false;
-            if (trace==1) printf( "   -> deleting cps[%i] = %i\n", j, cps[j] );
-            DeleteCPNode(cps[j],cp_root,&h);
-            h = false;
-            if (trace==1) printf( "   +> adding cps[%i] = %i\n", j, k_max+cps[j-1] );
-            if (trace==1) printf( "   o> [%u] (%u) [%u]\n", LB+cps[j-1], k_max+cps[j-1], RB+cps[j-1] );
-            AddCPNode( (size_t) k_max+cps[j-1], LB+cps[j-1], RB+cps[j-1], cp_root, &h );
-            cps[j] = (size_t) k_max+cps[j-1];
-            cpsl[j] = (size_t) LB+cps[j-1];
-            cpsr[j] = (size_t) RB+cps[j-1];
-            deletion_adjustment = true;
-            break;
-            }
-          }
-        else {
-          // remove this spurious point at index j
-          // increase the number of deleted point by 1
-          if (trace==1) printf( "  X=> removing cps[%i] = %i\n", j, cps[j] );
-          (*Ncpdlt)++;
-          DeleteCPNode(cps[j],cp_root,&h); // now, delete the cp node
-          // The next step may take a lot of time, using a bi-directional chain
-          // data structure may help to speed up the process.
-          for (i=j;i<=Ncp;i++) {
-            cps[i] = cps[i+1]; // move up the working array
-            cpsl[i] = cpsl[i+1];
-            cpsr[i] = cpsr[i+1];
-            }
-          Ncp--;
-          deletion_adjustment=true;
-          break;
-          }
-        // free up work space
-        free(Tk);
-        free(lambda1);
-        free(lambda2);
-        free(llrt);
-        free(u);
-        free(u1);
-        free(v2);
-        free(v21);
-        free(sk);
-        } // end of if n > 1
-      } // end of for j
-    } // end of while
-  free(cps);
-  free(cpsl);
-  free(cpsr);
-
-
-
-  }
-
-*/
